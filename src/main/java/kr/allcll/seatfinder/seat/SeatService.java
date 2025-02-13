b612d01fda6693542dd90b45267fddedc1c9ae20
Sumin <new.sumyang@gmail.com>
2025-02-14 01:27:48 +0900

    feat: 전체 수강 신청 전 과목 여석 제공 기능 추가

--- src/main/java/kr/allcll/seatfinder/seat/SeatService.java
+++ src/main/java/kr/allcll/seatfinder/seat/SeatService.java
@@ -1,12 +1,15 @@
 package kr.allcll.seatfinder.seat;
 
 import java.time.Duration;
+import java.time.LocalDate;
 import java.util.Date;
 import java.util.List;
 import java.util.concurrent.ScheduledFuture;
 import kr.allcll.seatfinder.pin.Pin;
 import kr.allcll.seatfinder.pin.PinRepository;
 import kr.allcll.seatfinder.pin.dto.PinSeatsResponse;
+import kr.allcll.seatfinder.seat.dto.PreSeatsResponse;
+import kr.allcll.seatfinder.seat.dto.SeatDto;
 import kr.allcll.seatfinder.seat.dto.SeatsResponse;
 import kr.allcll.seatfinder.sse.SseService;
 import kr.allcll.seatfinder.subject.Subject;
@@ -29,11 +32,12 @@ public class SeatService {
     private final SeatStorage seatStorage;
     private final PinRepository pinRepository;
     private final ThreadPoolTaskScheduler scheduler;
+    private final SeatRepository seatRepository;
 
     @Scheduled(fixedRate = 1000)
     public void sendNonMajorSeats() {
-        List<Seat> nonMajorSeats = seatStorage.getNonMajorSeats(NON_MAJOR_SUBJECT_QUERY_LIMIT);
-        sseService.propagate(NON_MAJOR_SEATS_EVENT_NAME, SeatsResponse.from(nonMajorSeats));
+        List<SeatDto> nonMajorSeatDtos = seatStorage.getNonMajorSeats(NON_MAJOR_SUBJECT_QUERY_LIMIT);
+        sseService.propagate(NON_MAJOR_SEATS_EVENT_NAME, SeatsResponse.from(nonMajorSeatDtos));
     }
 
     public void sendPinSeatsInformation(String token) {
@@ -42,11 +46,16 @@ public class SeatService {
             List<Subject> subjects = pins.stream()
                 .map(Pin::getSubject)
                 .toList();
-            List<Seat> pinSeats = seatStorage.getSeats(subjects);
-            sseService.propagate(PIN_EVENT_NAME, PinSeatsResponse.from(pinSeats));
+            List<SeatDto> pinSeatDtos = seatStorage.getSeats(subjects);
+            sseService.propagate(PIN_EVENT_NAME, PinSeatsResponse.from(pinSeatDtos));
         };
         ScheduledFuture<?> scheduledFuture = scheduler.scheduleAtFixedRate(task, Duration.ofMillis(TASK_DURATION));
         scheduler.schedule(() -> scheduledFuture.cancel(true),
             new Date(System.currentTimeMillis() + TASK_PERIOD));
+    }
+
+    public PreSeatsResponse getAllPreSeats() {
+        List<Seat> allByCreatedDate = seatRepository.findAllByCreatedDate(LocalDate.of(2025, 2, 14));
+        return PreSeatsResponse.from(allByCreatedDate);
     }
 }
