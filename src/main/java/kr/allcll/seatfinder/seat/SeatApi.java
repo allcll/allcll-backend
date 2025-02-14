7c45eb886dfdf9c0c47028f1f9bf63cc8245dd76
SuminKim <122220985+boyekim@users.noreply.github.com>
2025-02-14 10:42:00 +0900

    remove: 전체 수강 신청 전, 여석을 보여주는 기능 제거 (#74) (#75)
    Co-authored-by: Juhwan Kim <13selfesteem91@naver.com>

--- src/main/java/kr/allcll/seatfinder/seat/SeatApi.java
+++ src/main/java/kr/allcll/seatfinder/seat/SeatApi.java
@@ -1,20 +1,10 @@
 package kr.allcll.seatfinder.seat;
 
-import kr.allcll.seatfinder.seat.dto.PreSeatsResponse;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequiredArgsConstructor
 public class SeatApi {
 
-    private final SeatService seatService;
-
-    @GetMapping("/api/preSeat")
-    public ResponseEntity<PreSeatsResponse> getAllPreSeats() {
-        PreSeatsResponse response = seatService.getAllPreSeats();
-        return ResponseEntity.ok(response);
-    }
 }
