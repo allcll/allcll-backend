7fb12ece8de51abee04409f2bc6d82d50b492799
Sumin <new.sumyang@gmail.com>
2025-03-01 16:45:10 +0900

    chore: url 변경

--- src/main/java/kr/allcll/backend/admin/AdminController.java
+++ src/main/java/kr/allcll/backend/admin/AdminController.java
@@ -11,7 +11,7 @@ public class AdminController {
 
     private final AdminService adminService;
 
-    @PostMapping("/admin/set-sse-connection-open")
+    @PostMapping("/admin/sse-connect")
     public ResponseEntity<Void> sseConnect() {
         adminService.sseConnect();
         return ResponseEntity.ok().build();
