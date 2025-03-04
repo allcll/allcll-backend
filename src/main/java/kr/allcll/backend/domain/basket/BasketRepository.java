7bda3d2356f0e4dc0e2df9fc8b604349433fbe41
Sumin <new.sumyang@gmail.com>
2025-03-05 01:15:18 +0900

    refactor: BasketRepository 쿼리에 soft delete 설정 적용

--- src/main/java/kr/allcll/backend/domain/basket/BasketRepository.java
+++ src/main/java/kr/allcll/backend/domain/basket/BasketRepository.java
@@ -2,8 +2,14 @@ package kr.allcll.backend.domain.basket;
 
 import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface BasketRepository extends JpaRepository<Basket, Long> {
 
+    @Query("select b from Basket b "
+        + "join fetch b.subject s "
+        + "where s.id = :id "
+        + "and s.deletedAt is null "
+        + "and s.isDeleted = false")
     List<Basket> findBySubjectId(Long id);
 }
