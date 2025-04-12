f011c91c08cf30be2f0740a1c1070c7cbd05283c
Juhwan Kim <13selfesteem91@naver.com>
2025-04-12 16:43:41 +0900

    refactor: PinSeatSender 코드 개선 (#111)
    * refactor: 핀 과목 여석 조회를 SeatService로 분리
    
    * test: 핀 등록한 과목의 여석을 조회하는 테스트 작성
    
    * test: 비전공 과목 여석 데이터 PUSH 통합 테스트
    
    * test: 비전공 여석 SSE 통합 테스트를 비즈니스 정책과 동일하게 환경에서 테스트 작성
    
    * chore: 누락된 테스트명 수정

--- src/main/java/kr/allcll/backend/domain/seat/SeatService.java
+++ src/main/java/kr/allcll/backend/domain/seat/SeatService.java
@@ -3,6 +3,10 @@ package kr.allcll.backend.domain.seat;
 import java.time.LocalDate;
 import java.util.List;
 import kr.allcll.backend.domain.seat.dto.PreSeatsResponse;
+import kr.allcll.backend.domain.seat.dto.SeatDto;
+import kr.allcll.backend.domain.seat.pin.Pin;
+import kr.allcll.backend.domain.seat.pin.PinRepository;
+import kr.allcll.backend.domain.subject.Subject;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
@@ -13,9 +17,19 @@ import org.springframework.stereotype.Service;
 public class SeatService {
 
     private final SeatRepository seatRepository;
+    private final PinRepository pinRepository;
+    private final SeatStorage seatStorage;
 
     public PreSeatsResponse getAllPreSeats() {
         List<Seat> allByCreatedDate = seatRepository.findAllByCreatedDate((LocalDate.of(2025, 2, 28)));
         return PreSeatsResponse.from(allByCreatedDate);
+    }
+
+    public List<SeatDto> getPinSeats(String token) {
+        List<Pin> pins = pinRepository.findAllByToken(token);
+        List<Subject> subjects = pins.stream()
+            .map(Pin::getSubject)
+            .toList();
+        return seatStorage.getSeats(subjects);
     }
 }
