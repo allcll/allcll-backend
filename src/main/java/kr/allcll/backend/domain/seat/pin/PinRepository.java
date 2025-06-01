12a56f3745e723e1f3ce7511c25810baf07970f3
Sumin <new.sumyang@gmail.com>
2025-06-01 21:08:38 +0900

    refactor: 리뷰 반영

--- src/main/java/kr/allcll/backend/domain/seat/pin/PinRepository.java
+++ src/main/java/kr/allcll/backend/domain/seat/pin/PinRepository.java
@@ -12,15 +12,17 @@ public interface PinRepository extends JpaRepository<Pin, Long> {
         select p from Pin p
         join fetch p.subject s
         where p.token = :tokenId
-        and p.semesterAt = :semesterAt
         and s.isDeleted = false
+        and p.semesterAt = :semesterAt
         """)
     List<Pin> findAllByToken(String tokenId, String semesterAt);
 
     @Query(""" 
         select p from Pin p
+        join p.subject s
         where p.subject = :subject
         and p.token = :token
+        and s.isDeleted = false
         and p.semesterAt = :semesterAt
         """)
     Optional<Pin> findBySubjectAndToken(Subject subject, String token, String semesterAt);
