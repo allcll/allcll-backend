94913e36941485300bbd6ba7743fed16df9be22d
haeyoon1 <hynam1220@naver.com>
2025-07-15 23:50:19 +0900

    feat: TimeTableRepository 생성

---
+++ src/main/java/kr/allcll/backend/domain/timetable/TimeTableRepository.java
@@ -0,0 +1,27 @@
+package kr.allcll.backend.domain.timetable;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface TimeTableRepository extends JpaRepository<TimeTable, Long>{
+
+    @Query("""
+        SELECT t FROM TimeTable t 
+        WHERE t.token = :token
+        """)
+    Optional<List<TimeTable>> findByToken(@Param("token") String token);
+
+    @Query("""
+        SELECT t FROM TimeTable t 
+        WHERE t.id = :id 
+        AND t.token = :token
+        """)
+    Optional<TimeTable> findByIdAndToken(@Param("id") Long id, @Param("token") String token);
+}
