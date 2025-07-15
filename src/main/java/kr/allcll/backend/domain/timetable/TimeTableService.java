dfee8359bc8959502878f5d2f4ed8d7f5f4af110
haeyoon1 <hynam1220@naver.com>
2025-07-15 23:26:10 +0900

    feat: TimeTable service 작성

---
+++ src/main/java/kr/allcll/backend/domain/timetable/TimeTableService.java
@@ -0,0 +1,66 @@
+package kr.allcll.backend.domain.timetable;
+
+import kr.allcll.backend.domain.timetable.dto.TimeTableResponse;
+import kr.allcll.backend.domain.timetable.dto.TimeTablesResponse;
+import kr.allcll.backend.support.exception.AllcllErrorCode;
+import kr.allcll.backend.support.exception.AllcllException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@RequiredArgsConstructor
+public class TimeTableService {
+
+    private final TimeTableRepository timeTableRepository;
+
+    @Transactional(readOnly = true)
+    public TimeTablesResponse getTimetables(String token) {
+        List<TimeTable> timeTables = timeTableRepository.findByToken(token)
+                .orElseThrow(() -> new AllcllException(AllcllErrorCode.TOKEN_INVALID));
+        return new TimeTablesResponse(
+                timeTables.stream()
+                        .map(timeTable -> new TimeTableResponse(
+                                timeTable.getId(),
+                                timeTable.getTimeTableName(),
+                                timeTable.getSemester()))
+                        .toList()
+        );
+    }
+
+    @Transactional
+    public void createTimeTable(String token, String timeTableName, String semester) {
+        validateToken(token);
+
+        TimeTable timeTable = new TimeTable(token, timeTableName, semester);
+        timeTableRepository.save(timeTable);
+    }
+
+    @Transactional
+    public Object updateTimeTable(Long timetableId, String newTitle, String token) {
+        TimeTable timeTable = validateTimetable(timetableId, token);
+
+        timeTable.updateTimeTable(newTitle,);
+    }
+
+    @Transactional
+    public void deleteTimeTable(Long timetableId, String token) {
+        TimeTable timeTable = validateTimetable(timetableId, token);
+
+        timeTableRepository.delete(timeTable);
+    }
+
+    public void validateToken(String token) {
+        if (token == null || token.isBlank()) {
+            throw new AllcllException(AllcllErrorCode.TOKEN_INVALID);
+        }
+    }
+
+    public TimeTable validateTimetable(Long timetableId, String token) {
+        return timeTableRepository.findByIdAndToken(timetableId, token)
+                .orElseThrow(() -> new AllcllException(AllcllErrorCode.TIMETABLE_NOT_FOUND));
+    }
+
+}
