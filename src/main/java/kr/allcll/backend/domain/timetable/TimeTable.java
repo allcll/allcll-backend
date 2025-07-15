250dca04a525c44af361e37643277a7b3943c11e
haeyoon1 <hynam1220@naver.com>
2025-07-15 20:23:41 +0900

    feat: TimeTable 엔티티 생성

---
+++ src/main/java/kr/allcll/backend/domain/timetable/TimeTable.java
@@ -0,0 +1,36 @@
+package kr.allcll.backend.domain.timetable;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import kr.allcll.backend.support.entity.BaseEntity;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Table(name = "timetable")
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class TimeTable extends BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(name = "token")
+    private String token;
+
+    private String timeTableName;
+
+    private String semester;
+
+    public TimeTable(String token, String timeTableName, String semester) {
+        this.token = token;
+        this.timeTableName = timeTableName;
+        this.semester = semester;
+    }
+}
