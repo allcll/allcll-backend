c8552de91b3f4e1073d7b2b0f0ce6f9ce07464c9
haeyoon1 <hynam1220@naver.com>
2025-07-21 02:25:08 +0900

    feat: 에러코드 추가

--- src/main/java/kr/allcll/backend/support/exception/AllcllErrorCode.java
+++ src/main/java/kr/allcll/backend/support/exception/AllcllErrorCode.java
@@ -22,7 +22,8 @@ public enum AllcllErrorCode {
 
     TOKEN_INVALID("유효하지 않은 토큰입니다."),
     TIMETABLE_NOT_FOUND("시간표를 찾을 수 없습니다."),
-    UNAUTHORIZED_ACCESS("접근 권한이 없습니다.")
+    UNAUTHORIZED_ACCESS("접근 권한이 없습니다."),
+    INVALID_SEMESTER("유효하지 않은 학기입니다."),
     ;
 
     private String message;
