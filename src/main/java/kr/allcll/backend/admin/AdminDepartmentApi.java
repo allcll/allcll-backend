1e7859801e8b351b6821704a7c3ce5571b3e0691
haeyoon1 <hynam1220@naver.com>
2025-08-08 03:28:01 +0900

    feat: 검증 헤더 추가

--- src/main/java/kr/allcll/backend/admin/AdminDepartmentApi.java
+++ src/main/java/kr/allcll/backend/admin/AdminDepartmentApi.java
@@ -1,5 +1,6 @@
 package kr.allcll.backend.admin;
 
+import jakarta.servlet.http.HttpServletRequest;
 import kr.allcll.backend.domain.department.DepartmentService;
 import kr.allcll.backend.domain.department.dto.DepartmentsResponse;
 import kr.allcll.crawler.department.CrawlerDepartmentService;
@@ -16,18 +17,26 @@ public class AdminDepartmentApi {
 
     private final CrawlerDepartmentService crawlerDepartmentService;
     private final DepartmentService departmentService;
+    private final AdminRequestValidator validator;
 
     @PostMapping("/api/admin/departments")
-    public void fetchAndSaveDepartments(
+    public ResponseEntity<Void> fetchAndSaveDepartments(HttpServletRequest request,
         @RequestParam String userId,
         @RequestParam String year,
         @RequestParam String semesterCode
     ) {
+        if (validator.isRateLimited(request) || validator.isUnauthorized(request)) {
+            return ResponseEntity.status(401).build();
+        }
         crawlerDepartmentService.fetchAndSaveDepartments(userId, year, semesterCode);
+        return ResponseEntity.ok().build();
     }
 
     @GetMapping("/api/admin/departments")
-    public ResponseEntity<DepartmentsResponse> getAllDepartments() {
+    public ResponseEntity<DepartmentsResponse> getAllDepartments(HttpServletRequest request) {
+        if (validator.isRateLimited(request) || validator.isUnauthorized(request)) {
+            return ResponseEntity.status(401).build();
+        }
         DepartmentsResponse response = departmentService.retrieveAllDepartment();
         return ResponseEntity.ok(response);
     }
