6cb532f30bc889d488def8cf1a146cf1b90a139c
haeyoon1 <hynam1220@naver.com>
2025-08-07 23:41:41 +0900

    feat: 전체 학과 조회 api 추가

--- src/main/java/kr/allcll/backend/admin/AdminDepartmentApi.java
+++ src/main/java/kr/allcll/backend/admin/AdminDepartmentApi.java
@@ -1,7 +1,11 @@
 package kr.allcll.backend.admin;
 
+import kr.allcll.backend.domain.department.DepartmentService;
+import kr.allcll.backend.domain.department.dto.DepartmentsResponse;
 import kr.allcll.crawler.department.CrawlerDepartmentService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -11,13 +15,20 @@ import org.springframework.web.bind.annotation.RestController;
 public class AdminDepartmentApi {
 
     private final CrawlerDepartmentService crawlerDepartmentService;
+    private final DepartmentService departmentService;
 
-    @PostMapping("/api/admin/departments/fetch")
+    @PostMapping("/api/admin/departments")
     public void fetchAndSaveDepartments(
         @RequestParam String userId,
         @RequestParam String year,
         @RequestParam String semesterCode
     ) {
         crawlerDepartmentService.fetchAndSaveDepartments(userId, year, semesterCode);
+    }
+
+    @GetMapping("/api/admin/departments")
+    public ResponseEntity<DepartmentsResponse> getAllDepartments() {
+        DepartmentsResponse response = departmentService.retrieveAllDepartment();
+        return ResponseEntity.ok(response);
     }
 }
