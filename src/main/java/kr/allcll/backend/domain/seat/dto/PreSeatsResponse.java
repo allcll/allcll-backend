9a84c3d247d209fea8e42627b93d189470c7de1d
goldm0ng <jujs45177@gmail.com>
2025-08-15 00:18:35 +0900

    refacotr: PreSeat DTO 패키지 경로 변경

--- src/main/java/kr/allcll/backend/domain/seat/dto/PreSeatsResponse.java
+++ src/main/java/kr/allcll/backend/domain/seat/dto/PreSeatsResponse.java
@@ -1,14 +1,14 @@
 package kr.allcll.backend.domain.seat.dto;
 
 import java.util.List;
-import kr.allcll.backend.domain.seat.Seat;
+import kr.allcll.crawler.seat.PreSeatResponse;
 
 public record PreSeatsResponse(
-    List<PreSeatResponse> subjects
+    List<PreSeatResponse> preSeats
 ) {
 
-    public static PreSeatsResponse from(List<Seat> allSeats) {
-        List<PreSeatResponse> result = allSeats.stream()
+    public static PreSeatsResponse from(List<PreSeatResponse> allPreSeats) {
+        List<PreSeatResponse> result = allPreSeats.stream()
             .map(PreSeatResponse::from)
             .toList();
         return new PreSeatsResponse(result);
