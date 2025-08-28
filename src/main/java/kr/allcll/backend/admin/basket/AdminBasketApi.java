601c4c1ca513b26112382230c133f2ab128cecf2
haeyoon1 <hynam1220@naver.com>
2025-08-28 22:30:28 +0900

    refactor: admin토큰 검증 추가

--- src/main/java/kr/allcll/backend/admin/basket/AdminBasketApi.java
+++ src/main/java/kr/allcll/backend/admin/basket/AdminBasketApi.java
@@ -1,5 +1,6 @@
 package kr.allcll.backend.admin.basket;
 
+import jakarta.servlet.http.HttpServletRequest;
 import kr.allcll.backend.admin.AdminRequestValidator;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.ResponseEntity;
@@ -15,7 +16,11 @@ public class AdminBasketApi {
     private final AdminRequestValidator validator;
 
     @PostMapping("/api/admin/basket/fetch")
-    public ResponseEntity<Void> getSubjects(@RequestParam String userId) {
+    public ResponseEntity<Void> getSubjects(HttpServletRequest request,
+        @RequestParam String userId) {
+        if (validator.isRateLimited(request) || validator.isUnauthorized(request)) {
+            return ResponseEntity.status(401).build();
+        }
         adminBasketService.fetchAndSaveBaskets(userId);
         return ResponseEntity.ok().build();
     }
