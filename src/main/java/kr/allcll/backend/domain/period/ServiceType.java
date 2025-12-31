54af61b116bd03fccbefeb14ad2fc915a54b48ee
haeyoon1 <hynam1220@naver.com>
2025-12-31 10:00:00 +0900

    refactor: 미사용 메서드 삭제

--- src/main/java/kr/allcll/backend/domain/period/ServiceType.java
+++ src/main/java/kr/allcll/backend/domain/period/ServiceType.java
@@ -1,8 +1,5 @@
 package kr.allcll.backend.domain.period;
 
-import java.util.Arrays;
-import kr.allcll.backend.support.exception.AllcllErrorCode;
-import kr.allcll.backend.support.exception.AllcllException;
 import lombok.Getter;
 
 @Getter
@@ -18,12 +15,4 @@ public enum ServiceType {
     ServiceType(String value) {
         this.value = value;
     }
-
-    public static ServiceType fromValue(String value) {
-        return Arrays.stream(ServiceType.values())
-            .filter(s -> s.getValue().equals(value))
-            .findFirst()
-            .orElseThrow(() -> new AllcllException(AllcllErrorCode.INVALID_SERVICE_TYPE));
-    }
-    
 }
