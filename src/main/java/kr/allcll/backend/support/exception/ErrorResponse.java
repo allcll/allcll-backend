715b53dcee12d77eda267e25d44dbb2331b7b592
haeyoon1 <hynam1220@naver.com>
2026-01-23 18:20:12 +0900

    feat: 에러 응답 정팩메 생성

--- src/main/java/kr/allcll/backend/support/exception/ErrorResponse.java
+++ src/main/java/kr/allcll/backend/support/exception/ErrorResponse.java
@@ -1,8 +1,15 @@
 package kr.allcll.backend.support.exception;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+
 public record ErrorResponse(
     String code,
     String message
 ) {
+
+    public static ResponseEntity<ErrorResponse> of(final HttpStatus httpStatus, String code, String message) {
+        return ResponseEntity.status(httpStatus).body(new ErrorResponse(code, message));
+    }
 
 }
