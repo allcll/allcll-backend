f0bd57de2c79e188d2cb4ded5cc1aaa5b4b58935
Sumin <new.sumyang@gmail.com>
2026-01-27 02:40:34 +0900

    refactor: Batch 클래스 추상 클래스 상속

--- src/main/java/kr/allcll/backend/admin/seat/GeneralSeatBatch.java
+++ src/main/java/kr/allcll/backend/admin/seat/GeneralSeatBatch.java
@@ -1,48 +1,26 @@
 package kr.allcll.backend.admin.seat;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
+import kr.allcll.backend.support.batch.AbstractBatch;
 import kr.allcll.crawler.seat.CrawlerSeat;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
-public class GeneralSeatBatch {
+@RequiredArgsConstructor
+public class GeneralSeatBatch extends AbstractBatch<CrawlerSeat> {
 
     private static final int FLUSH_LIMIT = 15;
 
-    private final BlockingQueue<CrawlerSeat> generalSeatBatch;
     private final SeatPersistenceService seatPersistenceService;
-    private final Object lock;
 
-    public GeneralSeatBatch(SeatPersistenceService seatPersistenceService) {
-        this.generalSeatBatch = new LinkedBlockingQueue<>();
-        this.seatPersistenceService = seatPersistenceService;
-        this.lock = new Object();
+    @Override
+    protected int flushLimit() {
+        return FLUSH_LIMIT;
     }
 
-    public void saveGeneralSeatToBatch(CrawlerSeat crawlerSeat) {
-        generalSeatBatch.add(crawlerSeat);
-        if (generalSeatBatch.size() >= FLUSH_LIMIT) {
-            saveAll();
-        }
-    }
-
-    public List<CrawlerSeat> getAll() {
-        List<CrawlerSeat> flushResult = new ArrayList<>();
-        generalSeatBatch.drainTo(flushResult);
-        return flushResult;
-    }
-
-    private void saveAll() {
-        List<CrawlerSeat> crawlerSeats;
-        synchronized (lock) {
-            crawlerSeats = getAll();
-            if (crawlerSeats.isEmpty()) {
-                return;
-            }
-        }
-        seatPersistenceService.saveAllSeat(crawlerSeats);
+    @Override
+    protected void saveAll(List<CrawlerSeat> batch) {
+        seatPersistenceService.saveAllSeat(batch);
     }
 }
--- src/main/java/kr/allcll/backend/admin/seat/PinSeatBatch.java
+++ src/main/java/kr/allcll/backend/admin/seat/PinSeatBatch.java
@@ -1,48 +1,26 @@
 package kr.allcll.backend.admin.seat;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.BlockingQueue;
-import java.util.concurrent.LinkedBlockingQueue;
+import kr.allcll.backend.support.batch.AbstractBatch;
 import kr.allcll.crawler.seat.CrawlerSeat;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Component;
 
 @Component
-public class PinSeatBatch {
+@RequiredArgsConstructor
+public class PinSeatBatch extends AbstractBatch<CrawlerSeat> {
 
     private static final int FLUSH_LIMIT = 10;
 
-    private final BlockingQueue<CrawlerSeat> pinSeatBatch;
     private final SeatPersistenceService seatPersistenceService;
-    private final Object lock;
 
-    public PinSeatBatch(SeatPersistenceService seatPersistenceService) {
-        this.seatPersistenceService = seatPersistenceService;
-        this.pinSeatBatch = new LinkedBlockingQueue<>();
-        this.lock = new Object();
+    @Override
+    protected int flushLimit() {
+        return FLUSH_LIMIT;
     }
 
-    public void savePinSeatToBatch(CrawlerSeat crawlerSeat) {
-        pinSeatBatch.add(crawlerSeat);
-        if (pinSeatBatch.size() >= FLUSH_LIMIT) {
-            saveAll();
-        }
-    }
-
-    public List<CrawlerSeat> getAll() {
-        List<CrawlerSeat> flushResult = new ArrayList<>();
-        pinSeatBatch.drainTo(flushResult);
-        return flushResult;
-    }
-
-    private void saveAll() {
-        List<CrawlerSeat> crawlerSeats;
-        synchronized (lock) {
-            crawlerSeats = getAll();
-            if (crawlerSeats.isEmpty()) {
-                return;
-            }
-        }
-        seatPersistenceService.saveAllSeat(crawlerSeats);
+    @Override
+    protected void saveAll(List<CrawlerSeat> batch) {
+        seatPersistenceService.saveAllSeat(batch);
     }
 }
