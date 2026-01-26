cbb74df784c600f331d2a9660056a1d687e5b68f
Sumin <new.sumyang@gmail.com>
2026-01-26 10:40:18 +0900

    refactor: 스케줄러 삭제

--- src/main/java/kr/allcll/backend/admin/seat/BatchService.java
+++ src/main/java/kr/allcll/backend/admin/seat/BatchService.java
@@ -1,7 +1,5 @@
 package kr.allcll.backend.admin.seat;
 
-import java.time.Duration;
-import java.util.List;
 import kr.allcll.backend.support.scheduler.ScheduledTaskHandler;
 import kr.allcll.crawler.seat.CrawlerSeat;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -10,23 +8,17 @@ import org.springframework.stereotype.Service;
 @Service
 public class BatchService {
 
-    private static final Duration PIN_SEAT_SAVE_PERIOD = Duration.ofSeconds(1);
-    private static final Duration GENERAL_SEAT_SAVE_PERIOD = Duration.ofSeconds(3);
-
     private final PinSeatBatch pinSeatBatch;
     private final GeneralSeatBatch generalSeatBatch;
-    private final SeatPersistenceService seatPersistenceService;
     private final ScheduledTaskHandler scheduledTaskHandler;
 
     public BatchService(
         PinSeatBatch pinSeatBatch,
         GeneralSeatBatch generalSeatBatch,
-        SeatPersistenceService seatPersistenceService,
         @Qualifier("seatBatchHandler") ScheduledTaskHandler scheduledTaskHandler
     ) {
         this.pinSeatBatch = pinSeatBatch;
         this.generalSeatBatch = generalSeatBatch;
-        this.seatPersistenceService = seatPersistenceService;
         this.scheduledTaskHandler = scheduledTaskHandler;
     }
 
@@ -36,39 +28,6 @@ public class BatchService {
 
     public void saveGeneralSeatBatch(CrawlerSeat renewedCrawlerSeat) {
         generalSeatBatch.saveGeneralSeatToBatch(renewedCrawlerSeat);
-    }
-
-    public void flushAllSeatPeriodically() {
-        flushPinSeatPeriodically();
-        flushGeneralSeatPeriodically();
-    }
-
-    public void flushPinSeatPeriodically() {
-        scheduledTaskHandler.scheduleAtFixedRate(
-            flushPinSubject(),
-            PIN_SEAT_SAVE_PERIOD
-        );
-    }
-
-    public void flushGeneralSeatPeriodically() {
-        scheduledTaskHandler.scheduleAtFixedRate(
-            flushGeneralSubject(),
-            GENERAL_SEAT_SAVE_PERIOD
-        );
-    }
-
-    private Runnable flushPinSubject() {
-        return () -> {
-            List<CrawlerSeat> allCrawlerSeat = pinSeatBatch.getAll();
-            seatPersistenceService.saveAllSeat(allCrawlerSeat);
-        };
-    }
-
-    private Runnable flushGeneralSubject() {
-        return () -> {
-            List<CrawlerSeat> allCrawlerSeat = generalSeatBatch.getAll();
-            seatPersistenceService.saveAllSeat(allCrawlerSeat);
-        };
     }
 
     public void cancelFlushScheduling() {
