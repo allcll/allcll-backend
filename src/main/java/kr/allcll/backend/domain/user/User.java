b5a355671d2e1a942e5c40cb1e073661c8e30bed
haeyoon1 <hynam1220@naver.com>
2026-01-27 17:00:25 +0900

    feat: user 정보 저장하는 entity 생성

---
+++ src/main/java/kr/allcll/backend/domain/user/User.java
@@ -0,0 +1,55 @@
+package kr.allcll.backend.domain.user;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import kr.allcll.backend.support.exception.AllcllErrorCode;
+import kr.allcll.backend.support.exception.AllcllException;
+import lombok.AccessLevel;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Getter
+@NoArgsConstructor(access = AccessLevel.PROTECTED)
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String studentId;
+
+    private String name;
+
+    private String deptNm;
+
+    private int admissionYear;
+
+    //private String college;
+
+    //private ProgramType program_type;
+
+    private User(String studentId, String name, String deptNm, int admissionYear) {
+        this.studentId = studentId;
+        this.name = name;
+        this.deptNm = deptNm;
+        this.admissionYear = admissionYear;
+    }
+
+    public static User create(String studentId, String name, String dept_nm) {
+        return new User(studentId,
+            name,
+            dept_nm,
+            extractAdmissionYear(dept_nm));
+    }
+
+    private static int extractAdmissionYear(String studentId) {
+        if (studentId == null) {
+            throw new AllcllException(AllcllErrorCode.STUDENT_ID_FETCH_FAIL, studentId);
+        }
+        int year = Integer.parseInt(studentId.substring(0, 2));
+        return 2000 + year;
+    }
+}
