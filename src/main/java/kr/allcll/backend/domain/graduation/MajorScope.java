936ad6dc68a817ee3c18d6acb55b165a8a2e93c4
goldm0ng <jujs45177@gmail.com>
2026-01-31 22:05:11 +0900

    docs: MajorScope 주석 표현 개선

--- src/main/java/kr/allcll/backend/domain/graduation/MajorScope.java
+++ src/main/java/kr/allcll/backend/domain/graduation/MajorScope.java
@@ -1,6 +1,6 @@
 package kr.allcll.backend.domain.graduation;
 
-public enum MajorScope { // 기준이 적용되는 전공 형태
-    PRIMARY,        // 주전공
-    SECONDARY       // 복수전공
+public enum MajorScope { // 기준이 적용되는 전공 범위
+    PRIMARY,        // 주전공 기준
+    SECONDARY       // 복수전공 기준
 }
