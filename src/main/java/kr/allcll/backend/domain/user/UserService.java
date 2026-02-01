b9d14a0ab2b45c82f9201f3455d658b5d9e55e47
haeyoon1 <hynam1220@naver.com>
2026-02-01 21:45:03 +0900

    feat: 서비스 코드에 Transactional 어노테이션 추가

--- src/main/java/kr/allcll/backend/domain/user/UserService.java
+++ src/main/java/kr/allcll/backend/domain/user/UserService.java
@@ -11,8 +11,10 @@ import kr.allcll.backend.support.exception.AllcllErrorCode;
 import kr.allcll.backend.support.exception.AllcllException;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 @Service
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class UserService {
 
@@ -21,6 +23,7 @@ public class UserService {
     private final UserRepository userRepository;
     private final GraduationDepartmentInfoRepository departmentInfoRepository;
 
+    @Transactional
     public User findOrCreate(UserInfo info) {
         return userRepository.findByStudentId(info.studentId())
             .orElseGet(() -> save(info));
@@ -54,13 +57,14 @@ public class UserService {
             .orElseThrow(() -> new AllcllException(AllcllErrorCode.USER_NOT_FOUND));
     }
 
-
+    @Transactional
     public void update(Long userId, UpdateUserRequest updateUserRequest) {
         validateUserId(userId);
         User user = getById(userId);
         user.updateUser(updateUserRequest);
     }
 
+    @Transactional
     public void delete(Long userId) {
         validateUserId(userId);
         User user = getById(userId);
