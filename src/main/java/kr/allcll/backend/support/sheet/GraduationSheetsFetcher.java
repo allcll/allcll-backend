82185a0268d698226b70fae504b473c169b6e401
goldm0ng <jujs45177@gmail.com>
2026-02-01 14:21:32 +0900

    feat: Google Sheets 탭 이름 기반 데이터 조회 클래스 추가

---
+++ src/main/java/kr/allcll/backend/support/sheet/GraduationSheetsFetcher.java
@@ -0,0 +1,34 @@
+package kr.allcll.backend.support.sheet;
+
+import com.google.api.services.sheets.v4.Sheets;
+import com.google.api.services.sheets.v4.model.ValueRange;
+import java.util.List;
+import kr.allcll.backend.support.exception.AllcllErrorCode;
+import kr.allcll.backend.support.exception.AllcllException;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class GraduationSheetsFetcher {
+
+    public static final String RANGE = "!A:Z";
+
+    private final Sheets sheets;
+    private final GraduationSheetsProperties graduationSheetsProperties;
+
+    public GraduationSheetTable fetchAsTable(String tabName) {
+        try {
+            String range = tabName + RANGE;
+            ValueRange response = sheets.spreadsheets().values()
+                .get(graduationSheetsProperties.spreadsheetId(), range)
+                .execute();
+
+            List<List<Object>> values = response.getValues();
+            return GraduationSheetTable.from(values);
+        } catch (Exception e) {
+            throw new AllcllException(AllcllErrorCode.GOOGLE_SHEET_TAP_NOT_FOUND);
+        }
+    }
+}
+
