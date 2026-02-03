835e48ae35dd6aa122052418ed4a8ff42d6241f7
haeyoon1 <hynam1220@naver.com>
2026-02-03 11:02:26 +0900

    feat: 로그인 시 졸업인증정보도 저장하도록 로직 추가

--- src/main/java/kr/allcll/backend/domain/user/AuthFacade.java
+++ src/main/java/kr/allcll/backend/domain/user/AuthFacade.java
@@ -1,5 +1,8 @@
 package kr.allcll.backend.domain.user;
 
+import kr.allcll.backend.domain.graduation.check.GraduationCertFetcher;
+import kr.allcll.backend.domain.graduation.check.GraduationCertInfo;
+import kr.allcll.backend.domain.graduation.check.GraduationCertService;
 import kr.allcll.backend.domain.user.dto.LoginRequest;
 import kr.allcll.backend.domain.user.dto.LoginResult;
 import lombok.RequiredArgsConstructor;
@@ -12,14 +15,18 @@ public class AuthFacade {
 
     private final AuthService authService;
     private final UserFetcher userFetcher;
+    private final GraduationCertFetcher graduationCertFetcher;
     private final UserService userService;
+    private final GraduationCertService graduationCertService;
 
     public LoginResult login(LoginRequest loginRequest) {
         OkHttpClient client = authService.login(loginRequest);
 
-        UserInfo info = userFetcher.fetch(client);
+        UserInfo userInfo = userFetcher.fetch(client);
+        User user = userService.findOrCreate(userInfo);
 
-        User user = userService.findOrCreate(info);
+        GraduationCertInfo certInfo = graduationCertFetcher.fetch(client);
+        graduationCertService.createOrUpdate(user, certInfo);
 
         return new LoginResult(user.getId());
     }
