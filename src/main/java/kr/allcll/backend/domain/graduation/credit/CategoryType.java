a7ec9d8743e48b39efd6c4e600095b962e4b21ce
Sumin <new.sumyang@gmail.com>
2026-02-05 15:39:45 +0900

    refactor: CategoryType 추가

--- src/main/java/kr/allcll/backend/domain/graduation/credit/CategoryType.java
+++ src/main/java/kr/allcll/backend/domain/graduation/credit/CategoryType.java
@@ -7,6 +7,6 @@ public enum CategoryType { // 이수구분
     GENERAL_ELECTIVE,      // 교양선택
     MAJOR_REQUIRED,        // 전공필수
     MAJOR_ELECTIVE,        // 전공선택
+    MAJOR_BASIC,           // 전공기초
     ;
 }
-
