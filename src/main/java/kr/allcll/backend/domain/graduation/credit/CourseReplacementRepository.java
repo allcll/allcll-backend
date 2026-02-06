47b46fd3127127fb1f1dc2dcda5157d0e5f98f5b
goldm0ng <jujs45177@gmail.com>
2026-02-07 00:03:55 +0900

    feat: 필요한 엔티티 추가 세팅

---
+++ src/main/java/kr/allcll/backend/domain/graduation/credit/CourseReplacementRepository.java
@@ -0,0 +1,7 @@
+package kr.allcll.backend.domain.graduation.credit;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+public interface CourseReplacementRepository extends JpaRepository<CourseReplacement, Long> {
+
+}
