2ea361d17e9d3aec514dbbe910829d2d58bf1f59
goldm0ng <jujs45177@gmail.com>
2026-02-09 08:23:41 +0900

    refactor: Enum 타입 비교 null-safe한 방식으로 수정

--- src/main/java/kr/allcll/backend/domain/graduation/certification/MajorTargetTypeResolver.java
+++ src/main/java/kr/allcll/backend/domain/graduation/certification/MajorTargetTypeResolver.java
@@ -4,33 +4,39 @@ import kr.allcll.backend.domain.graduation.department.GraduationDepartmentInfo;
 
 public class MajorTargetTypeResolver {
 
-    static EnglishTargetType resolveEnglishTargetType(GraduationDepartmentInfo primaryDept, GraduationDepartmentInfo doubleDept) {
+    static EnglishTargetType resolveEnglishTargetType(GraduationDepartmentInfo primaryDept,
+        GraduationDepartmentInfo doubleDept) {
         if (doubleDept == null) {
             return primaryDept.getEnglishTargetType();
         }
         EnglishTargetType primaryDeptEnglishTargetType = primaryDept.getEnglishTargetType();
         EnglishTargetType doubleDeptEnglishTargetType = doubleDept.getEnglishTargetType();
 
-        if (primaryDeptEnglishTargetType == EnglishTargetType.ENGLISH_MAJOR || doubleDeptEnglishTargetType == EnglishTargetType.ENGLISH_MAJOR) {
+        if (EnglishTargetType.ENGLISH_MAJOR.equals(primaryDeptEnglishTargetType)
+            || EnglishTargetType.ENGLISH_MAJOR.equals(doubleDeptEnglishTargetType)) {
             return EnglishTargetType.ENGLISH_MAJOR;
         }
-        if (primaryDeptEnglishTargetType == EnglishTargetType.EXEMPT && doubleDeptEnglishTargetType == EnglishTargetType.EXEMPT) {
+        if (EnglishTargetType.EXEMPT.equals(primaryDeptEnglishTargetType)
+            && EnglishTargetType.EXEMPT.equals(doubleDeptEnglishTargetType)) {
             return EnglishTargetType.EXEMPT;
         }
         return EnglishTargetType.NON_MAJOR;
     }
 
-    static CodingTargetType resolveCodingTargetType(GraduationDepartmentInfo primaryDept, GraduationDepartmentInfo doubleDept) {
+    static CodingTargetType resolveCodingTargetType(GraduationDepartmentInfo primaryDept,
+        GraduationDepartmentInfo doubleDept) {
         if (doubleDept == null) {
             return primaryDept.getCodingTargetType();
         }
         CodingTargetType primaryDeptCodingTargetType = primaryDept.getCodingTargetType();
         CodingTargetType doubleDeptCodingTargetType = doubleDept.getCodingTargetType();
 
-        if (primaryDeptCodingTargetType == CodingTargetType.CODING_MAJOR || doubleDeptCodingTargetType == CodingTargetType.CODING_MAJOR) {
+        if (CodingTargetType.CODING_MAJOR.equals(primaryDeptCodingTargetType)
+            || CodingTargetType.CODING_MAJOR.equals(doubleDeptCodingTargetType)) {
             return CodingTargetType.CODING_MAJOR;
         }
-        if (primaryDeptCodingTargetType == CodingTargetType.EXEMPT && doubleDeptCodingTargetType == CodingTargetType.EXEMPT) {
+        if (CodingTargetType.EXEMPT.equals(primaryDeptCodingTargetType)
+            && CodingTargetType.EXEMPT.equals(doubleDeptCodingTargetType)) {
             return CodingTargetType.EXEMPT;
         }
         return CodingTargetType.NON_MAJOR;
