ffbbfa90fd4dac76fbca0841b9879abd3431889f
goldm0ng <jujs45177@gmail.com>
2026-02-08 15:23:29 +0900

    fix: 사용자의 학과 코드 조회 시 입학년도 필터링 조건을 추가하여 중복 문제 해결

--- src/main/java/kr/allcll/backend/domain/graduation/department/GraduationDepartmentInfoRepository.java
+++ src/main/java/kr/allcll/backend/domain/graduation/department/GraduationDepartmentInfoRepository.java
@@ -2,8 +2,10 @@ package kr.allcll.backend.domain.graduation.department;
 
 import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface GraduationDepartmentInfoRepository extends JpaRepository<GraduationDepartmentInfo, Long> {
 
-    Optional<GraduationDepartmentInfo> findByDeptNm(String deptNm);
+    @Query("SELECT g FROM GraduationDepartmentInfo g WHERE g.admissionYear = :admissionYear AND g.deptNm = :deptNm")
+    Optional<GraduationDepartmentInfo> findByAdmissionYearAndDeptNm(int admissionYear, String deptNm);
 }
