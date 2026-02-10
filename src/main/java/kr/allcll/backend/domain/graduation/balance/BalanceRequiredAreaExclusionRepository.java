5aa22373a4ce7b3245cb36e9be73456d956c7126
haeyoon1 <hynam1220@naver.com>
2026-02-10 17:08:53 +0900

    feat: 졸업요건 학점 검사 계산 로직 작성

--- src/main/java/kr/allcll/backend/domain/graduation/balance/BalanceRequiredAreaExclusionRepository.java
+++ src/main/java/kr/allcll/backend/domain/graduation/balance/BalanceRequiredAreaExclusionRepository.java
@@ -1,7 +1,13 @@
 package kr.allcll.backend.domain.graduation.balance;
 
+import java.util.List;
+import kr.allcll.backend.domain.graduation.department.DeptGroup;
 import org.springframework.data.jpa.repository.JpaRepository;
 
 public interface BalanceRequiredAreaExclusionRepository extends JpaRepository<BalanceRequiredAreaExclusion, Long> {
 
+    List<BalanceRequiredAreaExclusion> findByAdmissionYearAndDeptGroup(
+        Integer admissionYear,
+        DeptGroup deptGroup
+    );
 }
