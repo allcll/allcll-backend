586222952ff8c955b82a2ded4fc12bb3477108f4
haeyoon1 <hynam1220@naver.com>
2026-02-10 17:08:53 +0900

    feat: 졸업요건 학점 검사 계산 로직 작성

--- src/main/java/kr/allcll/backend/domain/graduation/balance/BalanceRequiredRuleRepository.java
+++ src/main/java/kr/allcll/backend/domain/graduation/balance/BalanceRequiredRuleRepository.java
@@ -1,7 +1,18 @@
 package kr.allcll.backend.domain.graduation.balance;
 
+import java.util.List;
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 public interface BalanceRequiredRuleRepository extends JpaRepository<BalanceRequiredRule, Long> {
 
+    Optional<BalanceRequiredRule> findByAdmissionYearAndDeptNm(
+        Integer admissionYear,
+        String deptNm
+    );
+
+    Optional<BalanceRequiredRule> findByAdmissionYearAndDeptNmIsNull(
+        Integer admissionYear
+    );
 }
