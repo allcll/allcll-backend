91116fd11dab7f2151a8b9543044f519dca69b47
haeyoon1 <hynam1220@naver.com>
2026-02-15 15:02:09 +0900

    refactor: 사용하지 않는 클래스 삭제

--- src/main/java/kr/allcll/backend/domain/graduation/check/result/dto/GraduationRecommendations.java
+++
@@ -1,9 +0,0 @@
-package kr.allcll.backend.domain.graduation.check.result.dto;
-
-import java.util.List;
-
-public record GraduationRecommendations(
-    List<RequiredCourseRecommendation> requiredCourses
-) {
-
-}
--- src/main/java/kr/allcll/backend/domain/graduation/check/result/dto/MissingCourse.java
+++
@@ -1,9 +0,0 @@
-package kr.allcll.backend.domain.graduation.check.result.dto;
-
-public record MissingCourse(
-    String curiNo,
-    String curiNm,
-    String altGroup
-) {
-
-}
