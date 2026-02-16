94ffbac117e7a06b934b6ebe6684b62854cd15ed
haeyoon1 <hynam1220@naver.com>
2026-02-16 15:02:47 +0900

    feat: 사용자의 균형교양 정보를 저장하는 보조 테이블 생성

---
+++ src/main/java/kr/allcll/backend/domain/graduation/check/result/GraduationCheckBalanceAreaResultRepository.java
@@ -0,0 +1,17 @@
+package kr.allcll.backend.domain.graduation.check.result;
+
+import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+
+public interface GraduationCheckBalanceAreaResultRepository extends JpaRepository<GraduationCheckBalanceAreaResult, Long> {
+
+    @Query("""
+        select r
+        from GraduationCheckBalanceAreaResult r
+        where r.userId = :userId
+    """)
+    List<GraduationCheckBalanceAreaResult> findAllByUserId(Long userId);
+
+    void deleteAllByUserId(Long userId);
+}
