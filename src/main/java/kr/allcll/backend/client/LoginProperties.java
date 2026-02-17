546d50f4dc73ab4728779a6be51a10e6272c7ad7
haeyoon1 <hynam1220@naver.com>
2026-02-17 12:40:21 +0900

    feat: tosc 로그인 요청 url 추가

--- src/main/java/kr/allcll/backend/client/LoginProperties.java
+++ src/main/java/kr/allcll/backend/client/LoginProperties.java
@@ -5,6 +5,7 @@ import org.springframework.boot.context.properties.ConfigurationProperties;
 @ConfigurationProperties(prefix = "spring.login")
 public record LoginProperties(
     String portalLoginUrl,
+    String toscLoginUrl,
     String portalLoginRedirectUrl,
     String portalLoginReferer,
     String studentInfoPageUrl,
