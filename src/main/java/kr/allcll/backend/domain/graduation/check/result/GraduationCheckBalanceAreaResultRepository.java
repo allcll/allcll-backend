b33fd44d80eccf32bc5b2fd93b03a85bbc5504c5
haeyoon1 <hynam1220@naver.com>
2026-02-17 09:58:02 +0900

    refactor: 불필요 메서드 삭제

--- src/main/java/kr/allcll/backend/domain/graduation/check/result/GraduationCheckBalanceAreaResultRepository.java
+++ src/main/java/kr/allcll/backend/domain/graduation/check/result/GraduationCheckBalanceAreaResultRepository.java
@@ -4,14 +4,13 @@ import java.util.List;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
 
-public interface GraduationCheckBalanceAreaResultRepository extends JpaRepository<GraduationCheckBalanceAreaResult, Long> {
+public interface GraduationCheckBalanceAreaResultRepository extends
+    JpaRepository<GraduationCheckBalanceAreaResult, Long> {
 
     @Query("""
-        select r
-        from GraduationCheckBalanceAreaResult r
-        where r.userId = :userId
-    """)
+            select r
+            from GraduationCheckBalanceAreaResult r
+            where r.userId = :userId
+        """)
     List<GraduationCheckBalanceAreaResult> findAllByUserId(Long userId);
-
-    void deleteAllByUserId(Long userId);
 }
