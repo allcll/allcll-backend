ba15cff93249e711c0ac5cc88b2e14367fc21149
goldm0ng <jujs45177@gmail.com>
2026-02-24 04:46:15 +0900

    refactor: 기본 트랜잭션을 readOnly로 설정

--- src/main/java/kr/allcll/backend/domain/graduation/check/result/CertificationChecker.java
+++ src/main/java/kr/allcll/backend/domain/graduation/check/result/CertificationChecker.java
@@ -17,6 +17,7 @@ import org.springframework.stereotype.Component;
 import org.springframework.transaction.annotation.Transactional;
 
 @Component
+@Transactional(readOnly = true)
 @RequiredArgsConstructor
 public class CertificationChecker {
 
