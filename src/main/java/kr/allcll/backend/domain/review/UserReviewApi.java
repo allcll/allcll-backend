6ff20115c1c6e1aab288c9653b4e3e78404145f2
haeyoon1 <hynam1220@naver.com>
2026-02-28 18:43:54 +0900

    feat: 사용자 리뷰 저장 컨트롤러 생성

---
+++ src/main/java/kr/allcll/backend/domain/review/UserReviewApi.java
@@ -0,0 +1,25 @@
+package kr.allcll.backend.domain.review;
+
+import kr.allcll.backend.domain.review.dto.UserReviewRequest;
+import kr.allcll.backend.support.web.Auth;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequiredArgsConstructor
+public class UserReviewApi {
+
+    private final UserReviewService userReviewService;
+
+    @PostMapping("api/review")
+    public ResponseEntity<Void> createReview(
+        @Auth Long userId,
+        @RequestBody UserReviewRequest userReviewRequest
+    ) {
+        userReviewService.createReview(userId, userReviewRequest);
+        return ResponseEntity.ok().build();
+    }
+}
