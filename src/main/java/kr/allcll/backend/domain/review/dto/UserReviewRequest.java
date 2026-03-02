9ab573d03d4053b2517a7ca77a43637711a33762
haeyoon1 <hynam1220@naver.com>
2026-03-02 11:58:09 +0900

    feat: 요청 dto 서비스타입 변수 형태 변경

--- src/main/java/kr/allcll/backend/domain/review/dto/UserReviewRequest.java
+++ src/main/java/kr/allcll/backend/domain/review/dto/UserReviewRequest.java
@@ -1,9 +1,11 @@
 package kr.allcll.backend.domain.review.dto;
 
+import kr.allcll.backend.domain.operationPeriod.OperationType;
+
 public record UserReviewRequest(
     Short rate,
     String detail,
-    String operationType
+    OperationType operationType
 ) {
 
 }
