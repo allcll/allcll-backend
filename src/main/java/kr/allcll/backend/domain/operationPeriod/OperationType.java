14e175a4a1e77cfc3b4954a1c8e926628f9c1698
haeyoon1 <hynam1220@naver.com>
2026-03-02 12:08:20 +0900

    feat: 사용되지 않는 필드 및 메서드 제거

--- src/main/java/kr/allcll/backend/domain/operationPeriod/OperationType.java
+++ src/main/java/kr/allcll/backend/domain/operationPeriod/OperationType.java
@@ -1,31 +1,19 @@
 package kr.allcll.backend.domain.operationPeriod;
 
-import java.util.Arrays;
-import kr.allcll.backend.support.exception.AllcllErrorCode;
-import kr.allcll.backend.support.exception.AllcllException;
 import lombok.Getter;
 
 @Getter
 public enum OperationType {
-    TIMETABLE("timetable", "시간표"),
-    BASKETS("baskets", "관심과목"),
-    SIMULATION("simulation", "올클연습"),
-    LIVE("live", "실시간여석"),
-    PRESEAT("preseat", "여석선공개"),
-    GRADUATION("graduation", "졸업요건검사");
+    TIMETABLE("timetable"),
+    BASKETS("baskets"),
+    SIMULATION("simulation"),
+    LIVE("live"),
+    PRESEAT("preseat"),
+    GRADUATION("graduation");
 
     private final String value;
-    private final String name;
 
-    OperationType(String value, String name) {
+    OperationType(String value) {
         this.value = value;
-        this.name = name;
-    }
-
-    public static OperationType from(String operationName) {
-        return Arrays.stream(values())
-            .filter(operationType -> operationType.value.equalsIgnoreCase(operationName.trim()))
-            .findFirst()
-            .orElseThrow(() -> new AllcllException(AllcllErrorCode.OPERATION_TYPE_NOT_FOUND));
     }
 }
