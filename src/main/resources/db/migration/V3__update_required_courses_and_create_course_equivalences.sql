e5bebacae75503a2944996c300689869e7063a74
goldm0ng <jujs45177@gmail.com>
2026-05-06 14:44:00 +0900

    feat: 변경된 DB 수정 사항에 대한 V3 스키마 추가

---
+++ src/main/resources/db/migration/V3__update_required_courses_and_create_course_equivalences.sql
@@ -0,0 +1,18 @@
+ALTER TABLE `required_courses`
+    RENAME COLUMN `alt_group` TO `same_course_code`;
+
+ALTER TABLE `required_courses`
+    ADD COLUMN `group_code` varchar(255) DEFAULT NULL;
+
+CREATE TABLE `course_equivalences` (
+  `id` bigint NOT NULL AUTO_INCREMENT,
+  `group_code` varchar(255) NOT NULL,
+  `curi_no` varchar(255) NOT NULL,
+  `curi_nm` varchar(255) NOT NULL,
+  `created_at` datetime(6) DEFAULT NULL,
+  `deleted_at` datetime(6) DEFAULT NULL,
+  `is_deleted` bit(1) NOT NULL,
+  `semester_at` varchar(255) DEFAULT NULL,
+  `updated_at` datetime(6) DEFAULT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
