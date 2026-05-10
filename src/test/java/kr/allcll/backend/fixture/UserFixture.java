853339fb2e3cae819bbdc5084e15b8e34fd06044
goldm0ng <jujs45177@gmail.com>
2026-05-10 11:28:41 +0900

    test: 사용자 별 졸업요건 상세 조회 테스트 추가

--- src/test/java/kr/allcll/backend/fixture/UserFixture.java
+++ src/test/java/kr/allcll/backend/fixture/UserFixture.java
@@ -8,13 +8,40 @@ public class UserFixture {
 
     private static final String DEFAULT_NAME = "allcll";
     private static final String DEFAULT_STUDENT_ID = "00000000";
+    private static final int DEFAULT_ADMISSION_YEAR = 2023;
+    private static final String DEFAULT_COLLEGE = "테스트대학";
+    private static final String DEFAULT_DEPT_NM = "테스트학과";
+    private static final String DEFAULT_DEPT_CD = "3220";
+
+    public static User singleMajorUser(String studentId) {
+        return new User(
+            studentId,
+            DEFAULT_NAME,
+            DEFAULT_ADMISSION_YEAR,
+            MajorType.SINGLE,
+            DEFAULT_COLLEGE,
+            DEFAULT_DEPT_NM,
+            DEFAULT_DEPT_CD,
+            null,
+            null,
+            null
+        );
+    }
 
     public static User singleMajorUser(
         int admissionYear,
         GraduationDepartmentInfo primaryuserDept
     ) {
+        return singleMajorUser(DEFAULT_STUDENT_ID, admissionYear, primaryuserDept);
+    }
+
+    public static User singleMajorUser(
+        String studentId,
+        int admissionYear,
+        GraduationDepartmentInfo primaryuserDept
+    ) {
         return new User(
-            DEFAULT_STUDENT_ID,
+            studentId,
             DEFAULT_NAME,
             admissionYear,
             MajorType.SINGLE,
