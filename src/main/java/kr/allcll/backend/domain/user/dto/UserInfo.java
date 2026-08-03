955ed480886f62027aa1315b6edef92ebab91312
Sumin <new.sumyang@gmail.com>
2026-08-03 17:23:29 +0900

    refactor: null 체크 제거

--- src/main/java/kr/allcll/backend/domain/user/dto/UserInfo.java
+++ src/main/java/kr/allcll/backend/domain/user/dto/UserInfo.java
@@ -28,16 +28,11 @@ public record UserInfo(
     }
 
     private static String normalizeDeptNm(String deptNm) {
-        if (deptNm == null) {
-            return null;
-        }
         return switch (deptNm) {
             case "연기예술", "연출제작" -> FILM_ART_DEPARTMENT;
             case "서양화", "한국화" -> PAINTING_DEPARTMENT;
-            case "성악", "피아노", "플루트", "클라리넷", "바이올린", "비올라", "첼로" ->
-                MUSIC_DEPARTMENT;
-            case "골프", "태권도", "축구", "리듬체조", "에어로빅체조", "사격", "수영", "양궁" ->
-                PHYSICAL_EDUCATION_DEPARTMENT;
+            case "성악", "피아노", "플루트", "클라리넷", "바이올린", "비올라", "첼로" -> MUSIC_DEPARTMENT;
+            case "골프", "태권도", "축구", "리듬체조", "에어로빅체조", "사격", "수영", "양궁" -> PHYSICAL_EDUCATION_DEPARTMENT;
             case "발레", "한국무용", "현대무용" -> DANCE_DEPARTMENT;
             default -> deptNm;
         };
